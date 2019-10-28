a475fc9067c0b6199bf9630a2a2adbd37a1ae27d
s15851 <s15851@pjwstk.edu.pl>
2019-10-28 17:34:36 +0100

    Time Stamps added to main class

--- TAU_Lab1/src/main/java/pl/szczepanik/tau_labs/domain/Boat.java
+++ TAU_Lab1/src/main/java/pl/szczepanik/tau_labs/domain/Boat.java
@@ -1,18 +1,29 @@
 package pl.szczepanik.tau_labs.domain;
 
+import java.util.Date;
+
 public class Boat {
 
     private int id;
     private String boatModel;
     private int yearOfProduction;
-
-    public Boat() {
-    }
+    private Date creationTime;
+	private Date modificationTime;
+	private Date readTime;
 
     public Boat(int id, String boatModel, int yearOfProduction) {
         this.id = id;
         this.boatModel = boatModel;
         this.yearOfProduction = yearOfProduction;
+    }
+
+    public Boat(int id, String boatModel, int yearOfProduction, Date creationTime, Date modificationTime, Date readTime) {
+        this.id = id;
+        this.boatModel = boatModel;
+        this.yearOfProduction = yearOfProduction;
+        this.creationTime = creationTime;
+        this.modificationTime = modificationTime;
+        this.readTime = readTime;
     }
 
     public int getId() {
@@ -37,6 +48,30 @@ public class Boat {
 
     public void setYearOfProduction(int yearOfProduction) {
         this.yearOfProduction = yearOfProduction;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public Date getModificationTime() {
+        return modificationTime;
+    }
+
+    public void setModificationTime(Date modificationTime) {
+        this.modificationTime = modificationTime;
+    }
+
+    public Date getReadTime() {
+        return readTime;
+    }
+
+    public void setReadTime(Date readTime) {
+        this.readTime = readTime;
     }
 
 }
