729311ab59a7bf205bb88dff23d4c1c4f91928d0
s15851 <s15851@pjwstk.edu.pl>
2019-10-28 19:59:33 +0100

    check read timestamp after read all - test failed

--- TAU_Lab1/src/test/java/pl/szczepanik/tau_labs/service/BoatServiceTest.java
+++ TAU_Lab1/src/test/java/pl/szczepanik/tau_labs/service/BoatServiceTest.java
@@ -131,7 +131,10 @@ public class BoatServiceTest {
 
     @Test
     public void checkTimeStampAfterReadAllRecords() {
-        when(timeSource.getCurrentDate()).thenReturn((long) 123456789);
+
+        long time = 123456789;
+
+        when(timeSource.getCurrentDate()).thenReturn((time));
         Boat boat1 = new Boat(21, "Antila 27", 2009);
         Boat boat2 = new Boat(22, "Tango 780", 2017);
         Boat boat3 = new Boat(23, "Twister", 2010);
@@ -140,14 +143,10 @@ public class BoatServiceTest {
         db.create(boat2);
         db.create(boat3);
         db.setTimeSource(timeSource.getCurrentDate());
-        db.readAll();
-        long time = 123456789;
-        System.out.println(db.read(21));
-        System.out.println(db.read(22));
-        System.out.println(db.read(23));
-        assertEquals(time, db.read(21).getReadTime());
-        assertEquals(time, db.read(22).getReadTime());
-        assertEquals(time, db.read(23).getReadTime());
+
+        assertEquals(time, db.readAll().getReadTime());
+        assertEquals(time, db.readAll().getReadTime());
+        assertEquals(time, db.readAll().getReadTime());
     }
 
 }
