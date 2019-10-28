dad723e21d15856c6627fcf1ac1895ecf10f2395
s15851 <s15851@pjwstk.edu.pl>
2019-10-28 19:50:25 +0100

    little corrections

--- TAU_Lab1/src/main/java/pl/szczepanik/tau_labs/interfaces/BoatInterface.java
+++ TAU_Lab1/src/main/java/pl/szczepanik/tau_labs/interfaces/BoatInterface.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface BoatInterface {
 
-    void create(Boat boat);
+    Boat create(Boat boat);
     Boat read(int id);
     List<Boat> readAll();
     Boat update(Boat boat);
